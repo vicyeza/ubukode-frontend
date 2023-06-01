d4206c4385828445a0e8b2a6f3c80e323d08278c
iaimable <airanzi@ric-house.rw>
2023-06-01 20:45:37 +0200

    Adding Dashboard

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {" "}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ex
+      amet vero qui vel tenetur porro ratione. Iusto labore laborum corporis
+      nostrum necessitatibus earum facilis ipsum hic est, nihil eius?
+    </div>
+  );
 };
 
 export default Home;
