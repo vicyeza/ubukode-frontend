4021f2d0ad20407fa75e4d29d335d9862b6ad00d
iaimable <airanzi@ric-house.rw>
2023-06-03 13:28:26 +0200

    Configurations

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import Chart from "../../components/chart/Chart";
-import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
-import "./home.css";
-import { userData } from "../../dummyData";
-import WidgetSm from "../../components/widgetSm/WidgetSm";
-import WidgetLg from "../../components/widgetLg/WidgetLg";
-
-export default function Home() {
-  return (
-    <div className="home">
-      <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-      <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
-      </div>
-    </div>
-  );
-}
-=======
 import React from "react";
 
 const Home = () => {
@@ -26,4 +5,3 @@ const Home = () => {
 };
 
 export default Home;
->>>>>>> b7a43cf (User Dashboard)
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,6 @@ import { Routes, Route } from "react-router-dom";
 // import Navbar from "../components/navbar/Navbar";
 import Signup from "../registrations/Signup";
 import Login from "../registrations/Login";
-<<<<<<< HEAD
-import Home from "../pages/Home";
-import Services from "../pages/Services";
-import Plans from "../pages/Plans";
-import Navbar from "../components/navbar/Navbar";
-import Features from "../pages/Features";
-import Footer from "../components/footer/Footer";
-import Contact from "../pages/Contact";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Dashboard from "../pages/Dashboard";
-=======
 import Home from "../pages/home/Home";
 import Services from "../pages/services/Services";
 import Plans from "../pages/plans/Plans";
@@ -33,7 +21,6 @@ import MetaData from "../components/dashboardLayout/metaData/MetaData";
 import Datas from "../components/dashboardLayout/datas/Datas";
 import Analysis from "../components/dashboardLayout/analyisis/Analysis";
 import NewUser from "../pages/forms/NewUser";
->>>>>>> b7a43cf (User Dashboard)
 
 const Index = () => {
   return (
@@ -48,9 +35,6 @@ const Index = () => {
         <Route exact path="/Plans" element={<Plans />}></Route>
         <Route exact path="/contact" element={<Contact />}></Route>
         <Route exact path="/Features" element={<Features />}></Route>
-<<<<<<< HEAD
-        <Route exact path="/dashboard" element={<Dashboard />}></Route>
-=======
         <Route exact path="/dashboard" element={<Dashboardlayout />}></Route>
         <Route element={<DashRoutes />}>
           <Route exact path="/dashboards" element={<Dashboard />}></Route>
@@ -61,7 +45,6 @@ const Index = () => {
           <Route exact path="/newUser" element={<NewUser />}></Route>
           <Route exact path="/metadata" element={<MetaData />}></Route>
         </Route>
->>>>>>> b7a43cf (User Dashboard)
       </Routes>
       <Footer />
     </>
